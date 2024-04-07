08dbeead07107b3ab6601910badbc4bc615cf9c2
tmznwnel00 <tmznwnel00@gmail.com>
2024-04-07 23:00:00 +0900

    feat: refactor signup with contrib.auth

--- server/app/views.py
+++ server/app/views.py
@@ -3,32 +3,42 @@ import json
 from firebase_admin import db
 
 import bcrypt
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def signup(request):
-    if request.method == 'POST':
+    if request.method == 'POST':    
         data = json.loads(request.body)
-        new_data = data.get('new_user')
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
         
-        if new_data:
-            username = new_data.get('username')
-            id = new_data.get('id')
-            password = new_data.get('password')
-            
-            ref = db.reference('/users')
-            snapshot = ref.order_by_key().equal_to(username).get()
-            if snapshot:
-                return JsonResponse({'error': 'Username is already in use'}, status=400)
-            
-            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-            ref.child(username).set({
-                'id': id,
-                'password': hashed_password
-            })
+        if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+            return JsonResponse({'error': 'Username is already in use'}, status=400)
 
-            return JsonResponse({'message': 'User created'})
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        user = User.objects.create_user(username=username, email=email, password=hashed_password)
+                    
+        ref = db.reference('/users')
+        ref.child(username).set({
+            'email': email,
+            'password': hashed_password
+        })
+        return JsonResponse({'message': 'User created'})
 
     return JsonResponse({'error': 'Invalid request or missing data'}, status=400)
         
+# @csrf_exempt
+# def login(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body)
+#         new_data = data.get('login_user')        
+        
+#         if new_data:            
+#             id = new_data.get('id')
+#             password = new_data.get('password')
+#             return JsonResponse({'message': 'User created'})
+
+#     return JsonResponse({'error': 'Invalid request or missing data'}, status=400)
--- server/server/urls.py
+++ server/server/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from app.views import signup
 
 urlpatterns = [
-    path('signup', signup, name='signup')
+    path('signup', signup, name='signup'),
+    path('login', signup, name='login')
 ]
