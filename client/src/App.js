11c48036b35f55990f766a1a043652c40d73315d
tmznwnel00 <tmznwnel00@gmail.com>
2024-04-14 00:04:12 +0900

    fix: redirect to MainPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,5 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MainPage from './src/screens/MainPage';
-import LoginPage from './src/screens/LoginPage';
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-const Stack = createStackNavigator();
+import MainPage from './screens/MainPage';
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Main" component={MainPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+registerRootComponent(MainPage);
