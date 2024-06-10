9c335fc775a4389dcd6e18ed09b8376bedef81b4
LSM2461 <165548637+LSM2461@users.noreply.github.com>
2024-06-10 18:06:48 +0900

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -407,9 +407,9 @@ def user_books_analysis(request):
     for category, count in category_count.items():
         pie_chart.add(category, count)
 
-    pie_chart.config(font_family='googlefont:Raleway', label_font_size=100, major_label_font_size=100, value_font_size=100)
+    pie_chart.config(font_family='googlefont:Raleway', label_font_size=300, major_label_font_size=300, value_font_size=300)
     pie_chart.legend_at_bottom = True
-    pie_chart.legend_box_size = 100
+    pie_chart.legend_box_size = 50
 
     return HttpResponse(pie_chart.render(), content_type='image/svg+xml')
 
